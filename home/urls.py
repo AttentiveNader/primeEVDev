b7d20aa5b5c98b085f189c0e81008f221fe3bc78
mAshrafDawood <ashraf.dawood2295@gmail.com>
2021-07-26 02:04:53 +0200

    Removed forms & Added TODO statements

--- home/urls.py
+++ home/urls.py
@@ -9,8 +9,8 @@ app_name = ''
 urlpatterns = [
     path('', views.home, name='home' ),
     path('contactUs/', views.contactUs, name='contactUs'),
-    path('Sign-Up/', views.signUp , name='signUp'),
-    path('Sign-In/', auth_views.LoginView.as_view(template_name='registration/LogIn.html'), name='login'),
+    path('Sign-Up/', views.signUp, name='signUp'),
+    path('Sign-In/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='homePage/home.html'), name="logout"),
     path('aboutUs/',views.aboutUs, name='aboutUs'),
     path('info/',views.info, name='info')
--- home/views.py
+++ home/views.py
@@ -6,6 +6,18 @@ from .forms import UserRegistrationForm
 # Create your views here.
 
 @never_cache
+def login(request):
+    if request.method == 'POST':
+        """
+            TODO
+            1. Extract all the data from request
+            2. Query DB for authentication
+            3. Login using this user
+            4. Redirect to login with error message OR Redirect to home accordingly
+        """
+    return render(request, 'registration/LogIn.html')
+
+@never_cache
 def home(request):
     return render(request,'homePage/home.html')
 
@@ -15,11 +27,12 @@ def contactUs(request):
 
 @never_cache
 def signUp(request):
-    return render(request, 'logInSignUp/SignUp.html')
+    return render(request, 'registration/SignUp.html')
 
 @never_cache
 def aboutUs(request):
     return render(request, 'homePage/aboutUs.html')
+
 @never_cache
 def info(request):
     return render(request, 'homePage/info.html')
@@ -27,11 +40,14 @@ def info(request):
 @never_cache
 def signUp(request):
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = UserRegistrationForm()
+        """
+            TODO
+            1. Extract all the data from request
+            2. Query DB for authentication
+            3. Create User
+            4. Save User
+            5. Login using this user
+        """
+        return redirect('home')
     return render(request, 'registration/SignUp.html', {'form': form})
 
