6907b6532fb7cffda750348ab08fe486039bbf3f
NaderJS <nader_atef80@outlook.com>
2021-12-09 00:03:05 +0200

    formValidation phone number

--- static/js/info.js
+++ static/js/info.js
@@ -1,7 +1,14 @@
 function validateNB(inputtxt) {
     var phoneno = /^01[0-2,5]{1}[0-9]{8}$/;
-    var phoneno2 = /^+201[0-2,5]{1}[0-9]{8}$/;
-    if (!(inputtxt.value.match(phoneno)) && !(inputtxt.value.match(phoneno2))){return true;}
-    else{ alert("Invalid Phone number");
+    var phoneno2 = /^\+201[0-2,5]{1}[0-9]{8}$/;
+    if ( (inputtxt.value.match(phoneno)) || (inputtxt.value.match(phoneno2))){return true;}
+    else{
     return false;}
 }
+
+document.getElementById("infoForm").addEventListener("submit",(e)=>{
+   if(!validateNB(document.getElementById("nb"))){
+       e.preventDefault()
+       alert("invalid phone number")
+   }
+});
