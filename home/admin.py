50dbc3bf7250ea0e880e6247b645e419d8c5ce32
mAshrafDawood <ashraf.dawood2295@gmail.com>
2021-12-08 20:44:49 +0200

    Added Products Models

--- home/admin.py
+++ home/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, Product
 
 # Register your models here.
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone_number', 'is_male', 'car_brand', 'car_type', 'battery_range', 'license', 'has_card')
+    
     """def has_add_permission(self, request):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False"""
+        
     list_filter = ("has_card",)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'socket_type', 'number_of_sockets', 'charging_power', 'protection', 'ware_number', 'category', 'price', 'code')
 
 
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Product, ProductAdmin)
